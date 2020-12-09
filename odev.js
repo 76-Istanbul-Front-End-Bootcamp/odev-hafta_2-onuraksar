5c5d30d9194d51b3169d2e797d2764dc5b7f1f71
Onur AKŞAR <onuraksar96@gmail.com>
2020-12-09 22:47:47 +0000

    Initial Commit

--- odev.js
+++ odev.js
@@ -1,7 +1,7 @@
-/*
-  Odev 1:
-  Asagidaki fonksiyonu "Hello, John ciktisi verecek sekilde duzenleyiniz."
- */
+// /*
+//   Odev 1:
+//   Asagidaki fonksiyonu "Hello, John ciktisi verecek sekilde duzenleyiniz."
+//  */
 
 var person = {
   name: "John",
@@ -10,8 +10,8 @@ var person = {
   }
 }
 
-var messageFunc = person.message
-messageFunc();
+var messageFunc = person.message.bind(person);  // insert the person object as "this" into the function expression.
+messageFunc();  
 
 
 /*  
@@ -25,13 +25,13 @@ messageFunc();
 var numbers = {
   numbers: [[10,20,30], 2],
   multiply: function(){
-    this.numbers[0].map(function(number, numberIndex){
+    this.numbers[0].map((number, numberIndex) => {
         const result = number * this.numbers[1];
         console.log(result)
-    })
+    });
   }
 };
-
+// by turning map function into an arrow function, "this" will refer to parent scope as it's supposed to, for our case.
 numbers.multiply();
 
 
@@ -43,8 +43,22 @@ numbers.multiply();
   Ornek : isValidName(" J ohn") false donmeli
 */
 function isValidName(name){
-
+    if(typeof(name) === "string") {
+        let trimmedString = name.trim(); // delete all the spaces from both sides.
+        let splittedStringArray = trimmedString.split(" "); // split the string into substrings inside an Array.
+        for(let i = 0; i<splittedStringArray.length; i++) {
+            if(splittedStringArray[i].length > 1) {
+                return true // this is a valid name if the string contains more than 1 letter logically...
+            } else {
+                return false // otherwise, single letter string doesn't make sense as a valid name.
+            }
+        }
+    } else {
+        console.log( name + " is not even a string!"); 
+        return false;
+        }
 }
+
 
 /*
   Odev 4:
@@ -58,8 +72,17 @@ function isValidName(name){
   Ornek: katilimSaati("3", 20) 60 sonucunu vermelidir.
   Ornek: katilimSaati("5", "30") 150 sonucunu vermelidir.
 */
-function katilimSaati(dersSayisi, dersSuresi){
 
+
+function katilimSaati(dersSayisi, dersSuresi){
+    let nonValidEntries = [true,false,null,undefined,NaN,""]; // all the possible non-numbers.
+    const checkExists = (item) => {return item === dersSayisi || item === dersSuresi}; // function for .some method.
+  
+    if(nonValidEntries.some(checkExists) || isNaN(dersSayisi) || isNaN(dersSuresi)) {
+        console.log("Your entries are not a number.");
+    } else {
+        return dersSayisi * dersSuresi;
+    }
 }
 
 
